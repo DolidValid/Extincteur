a8f3d6ab5dac55755274d35dde24a8aea34415f6
DolidValid <brahamiawalid@yahoo.com>
2022-06-05 15:56:57 +0100

    v8.1

--- model/model_login.php
+++ model/model_login.php
@@ -5,7 +5,28 @@ class user
     private $pseudo;
     private $password;
 
+public function login ($pseudo,$password){
 
+    $req = $bdd->prepare('SELECT pseudo , passeword FROM user WHERE pseudo = :pseudo
+    AND pass = :pass');
+    $req->execute(array(
+    'pseudo' => $pseudo,
+    'passeword' => $password));
+    $resultat = $req->fetch();
+
+    if (!$resultat)
+    {
+    return false 
+    }else{
+    session_start();
+    
+    $_SESSION['pseudo'] = $pseudo;
+    return true 
+
+    }
+
+
+}
 
 
 
