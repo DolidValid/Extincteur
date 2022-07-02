a2291a71a03da4cb412fe7e266bc5b0403287a02
DolidValid <brahamiawalid@yahoo.com>
2022-07-02 23:36:39 +0100

    good job

--- views/view_login.php
+++ views/view_login.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>logIn</title>
+    <title>
+    Clinique IMEN
+      </title>
+      <link rel="icon" type="image/png" href="ASSETS/IMG/ongle.PNG" />
+     
     <link rel="stylesheet" href="../ASSETS/CSS/bootstrap-icons.css">   
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-
-    <link href="../ASSETS/CSS/bootstrap.min.css" rel="stylesheet">
         
+    <link href="../ASSETS/CSS/bootstrap.min.css" rel="stylesheet">
+    
 
     <style>
       .bd-placeholder-img {
